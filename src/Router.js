56f7ec462315245476be24b884062e05f73f7928
hymn9611 <hymn9611@naver.com>
2022-05-11 17:40:07 +0900

    modify: 초기 셋팅 피드백 적용

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,51 @@
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// 김형석
+import HengxiLogin from './pages/Hengxi/Login/Login.js';
+import HengxiMain from './pages/Hengxi/Main/Main.js';
+
+// 최현민
+import HyunminChoiLogin from './pages/hyunminChoi/Login/Login.js';
+import HyunminChoiMain from './pages/hyunminChoi/Main/Main.js';
+
+// 염종은
+import JongeunLogin from './pages/jongeun/Login/Login.js';
+import JongeunMain from './pages/jongeun/Main/Main.js';
+
+// 박주영
+import JooyongParkLogin from './pages/jooyoungPark/Login/Login.js';
+import JooyongParkMain from './pages/jooyoungPark/Main/Main.js';
+
+// 김정준
+import JungjunKimLogin from './pages/jungjunKim/Login/Login.js';
+import JungjunKimMain from './pages/jungjunKim/Main/Main.js';
+
+// 장수연
+import SooyeonJangLogin from './pages/sooyeonJang/Login/Login.js';
+import SooyeonJangMain from './pages/sooyeonJang/Main/Main.js';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
-        <Route path="/main" />
-        <div>sad</div>
+        <Route path="/" element={<HengxiLogin />} />
+        <Route path="/main-hs" element={<HengxiMain />} />
+
+        <Route path="/login-hm" element={<HyunminChoiLogin />} />
+        <Route path="/main-hm" element={<HyunminChoiMain />} />
+
+        <Route path="/login-ju" element={<JongeunLogin />} />
+        <Route path="/main-ju" element={<JongeunMain />} />
+
+        <Route path="/login-jy" element={<JooyongParkLogin />} />
+        <Route path="/main-jy" element={<JooyongParkMain />} />
+
+        <Route path="/login-jj" element={<JungjunKimLogin />} />
+        <Route path="/main-jj" element={<JungjunKimMain />} />
+
+        <Route path="/login-sy" element={<SooyeonJangLogin />} />
+        <Route path="/main-sy" element={<SooyeonJangMain />} />
       </Routes>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './styles/reset.scss';
 import './styles/common.scss';
 import Router from './Router.js';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
-
-// reportWebVitals();
