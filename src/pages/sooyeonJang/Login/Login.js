be3f4683f4409557a0e8336527c58c422822546e
Rey <Rey@Reys-MacBook-Pro.local>
2022-05-12 14:23:39 +0900

    ADD: login-sy/main-sy

--- src/pages/sooyeonJang/Login/Login.js
+++ src/pages/sooyeonJang/Login/Login.js
@@ -1,46 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Login.scss"; //link css
-import { useNavigate } from "react-router-dom";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.scss'; //link css
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/main-sy');
+  };
 
-    const goToMain = () => {
-      navigate('/main')
-    }
-
-    return (
-        <>
-        <div className="login-container">
+  return (
+    <>
+      <div className="login-container">
         <h1>Westagram</h1>
         <form>
-            <input type="text" placeholder="Phone number, username or email address" className="login-input" id="id" />
-            <input type="password" placeholder="Password" className="login-input" id="pw" onkeydown="activateBtn()" />
-            <button className="login-btn" type="submit" onClick={goToMain}>Log In</button>
+          <input
+            type="text"
+            placeholder="Phone number, username or email address"
+            className="login-input"
+            id="id"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="login-input"
+            id="pw"
+            onkeydown="activateBtn()"
+          />
+          <button className="login-btn" type="submit" onClick={goToMain}>
+            Log In
+          </button>
         </form>
         <div className="divider">OR</div>
         <div className="fb-wrapper">
-            <div className="fb">
-            <a href="https://facebook.com"><img src="images/facebook.png" alt="facebook-logo" />
-                Log in with Facebook
+          <div className="fb">
+            <a href="https://facebook.com">
+              <img src="/images/sooyeonJang/facebook.png" alt="facebook-logo" />
+              Log in with Facebook
             </a>
-            </div>
+          </div>
         </div>
         <div className="forgot-wrapper">
-            <div className="forgot">
-            <a href="https://www.instagram.com/accounts/password/reset/">Forgotten your password?</a>
-            </div>
+          <div className="forgot">
+            <a href="https://www.instagram.com/accounts/password/reset/">
+              Forgotten your password?
+            </a>
+          </div>
         </div>
-        </div>
-        <div className="sign-up">
-        <p>Don't have an account? <a href="https://www.instagram.com/accounts/emailsignup/">Sign up</a></p>
-        </div>
-    </>  
-    );
-}
-
+      </div>
+      <div className="sign-up">
+        <p>
+          Don't have an account?{' '}
+          <a href="https://www.instagram.com/accounts/emailsignup/">Sign up</a>
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default Login;
