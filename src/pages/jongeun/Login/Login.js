af16c8f591952fefbc0b55a20ed11de9ba665a61
Yumjong <yumjongeun@gmail.com>
2022-05-12 21:57:10 +0900

    [33기 염종은]Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jongeun/Login/Login.js
+++ src/pages/jongeun/Login/Login.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './Login.scss';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function Login() {
   const navigate = useNavigate();
   const goToMain = () => {
     navigate('/main-ju');
   };
+
+  function handleIdInput(e) {
+    setId(e.target.value);
+    console.log('id', id);
+  }
+
+  function handlePwInput(e) {
+    setPw(e.target.value);
+    console.log('pw', pw);
+  }
+
+  let [id, setId] = useState('');
+  let [pw, setPw] = useState('');
+
   return (
     <div>
       <div className="section">
@@ -14,11 +29,13 @@ function Login() {
           <div className="title">Westagram</div>
           <div className="input_area">
             <input
+              onChange={handleIdInput}
               className="id_input"
               type="text"
               placeholder="전화번호,사용자 이름 또는 이메일"
             />
             <input
+              onChange={handlePwInput}
               className="pw_input"
               type="password"
               placeholder="비밀번호"
