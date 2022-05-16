75ef0a098d334bb7996e07cdb6ebdff4cc4c8749
김정준 <jjkm0222@gmail.com>
2022-05-16 10:28:07 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/jungjunKim/Login/Login.js
+++ src/pages/jungjunKim/Login/Login.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useState } from 'react';
 
+// mission 2
+import { useNavigate } from 'react-router-dom';
+
 import './Login.scss';
 
 function JungjunKimLogin() {
   let [inputIdValue, setInputIdValue] = useState('');
   let [inputPwValue, setInputPwValue] = useState('');
+  // mission 2
+  let navigate = useNavigate();
 
   let handleIdInput = event => {
     setInputIdValue(event.target.value); // id태그에 입력된 값이 state로 저장
@@ -44,7 +49,19 @@ function JungjunKimLogin() {
           />
           {/* <p className="emailCheck">id : '@', '.' 포함 / pw : 5글자 이상</p> */}
         </div>
-        <button className="loginBtn">로그인</button>
+        {/**mission 2) 로그인 버튼 활성화 */}
+        {inputIdValue.includes('@') && inputPwValue.length >= 5 ? (
+          <button
+            className="loginBtnValid"
+            onClick={() => {
+              navigate('/main-jj');
+            }}
+          >
+            로그인
+          </button>
+        ) : (
+          <button className="loginBtn">로그인</button>
+        )}
         <div>
           <a href="#">비밀번호를 잊으셨나요?</a>
         </div>
